a3144595b8b51f42bc8037df14ef9bdd220eb842
DylanAndrew88 <Dandrewblake88@gmail.com>
2017-07-26 11:16:30 -0400

    record placeholder

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-/*
-  Here is a guide for the steps you could take:
-*/
-
-// 1. First select and store the elements you'll be working with
-
-
-// 2. Create your `onSubmit` event for getting the user's search term
-
-
-// 3. Create your `fetch` request that is called after a submission
-
-
-// 4. Create a way to append the fetch results to your page
-
-
-// 5. Create a way to listen for a click that will play the song in the audio play
 
   let container = document.getElementById('user-input');
   let submission = document.getElementById('submission');
@@ -33,12 +16,16 @@
         response.json().then(function(data) {
           placeholder.innerHTML = "";
           for(i=0; i < data.length; i++) {
+          if (data[i].artwork_url == null) {
+             data[i].artwork_url = "record_placeholder.png";
+          }
           let markup = `
             <div id="results">
               <img id="track-art" src="${data[i].artwork_url}">
                <p>${data[i].title}</p>
                <p id="user-name">${data[i].user.username}</p>
                <p>
+               <br>
                <audio id="music-player" controls="controls" src="${data[i].stream_url}/?client_id=8538a1744a7fdaa59981232897501e04&"></audio>
                </p>
             </div>
