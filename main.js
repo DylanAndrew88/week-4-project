a5dd4320c4fa2f607db29f38afdefd250254867e
DylanAndrew88 <Dandrewblake88@gmail.com>
2018-02-11 22:57:20 -0500

    added margin to input box

--- main.js
+++ main.js
@@ -10,6 +10,7 @@
     document.getElementsByClassName('sub-title')[0].style.marginBottom = "-100px";
     document.getElementById('submission').style.display = "inline";
     document.getElementById('submission').style.width = "35vw";
+    document.getElementById('submission').style.marginRight = "2vw";
     let string = submission.value;
     fetch('https://api.soundcloud.com/tracks?client_id=8538a1744a7fdaa59981232897501e04&q=' + string)
     .then(
@@ -26,15 +27,46 @@
           }
           let markup = `
             <div id="results">
-              <img id="track-art" src="${data[i].artwork_url}">
-               <p class="title">${data[i].title}</p>
-               <p class="userName">${data[i].user.username}</p>
-               <p>
-               <audio id="music-player" controls="controls" src="${data[i].stream_url}/?client_id=8538a1744a7fdaa59981232897501e04&"></audio>
-               </p>
+              <a onclick="soloTrack"><img id="track-art" src="${data[i].artwork_url}"></a>
+              <p id="title">${data[i].title}</p>
+              <p id="userName">${data[i].user.username}</p>
+              <p>
+              <audio id="music-player" controls="controls" src="${data[i].stream_url}/?client_id=8538a1744a7fdaa59981232897501e04&"></audio>
+              </p>
             </div>
         `
         placeholder.innerHTML += markup;
         }
+        console.log(document.getElementById('title'));
 
-      })})}
+      })})};
+
+  function soloTrack() {
+    console.log(document.getElementById('title'));
+    // fetch('https://api.soundcloud.com/tracks?client_id=8538a1744a7fdaa59981232897501e04&q=')
+    // .then(
+    //   function(response) {
+    //     if(response.status !== 200) {
+    //       console.log('Looks like there was a problem. Status Code: ' + response.status);
+    //       return
+    //     }
+    //     response.json().then(function(data) {
+    //       placeholder.innerHTML = "";
+    //       if (data[i].artwork_url == null) {
+    //          data[i].artwork_url = "record_placeholder.png";
+    //       }
+    //       let markup = `
+    //         <div id="results">
+    //           <a href="single.html" onclick>="soloTrack"<img id="track-art" src="${data[i].artwork_url}"></a>
+    //           <p class="title">${data[i].title}</p>
+    //           <p class="userName">${data[i].user.username}</p>
+    //           <p>
+    //           <audio id="music-player" controls="controls" src="${data[i].stream_url}/?client_id=8538a1744a7fdaa59981232897501e04&"></audio>
+    //           </p>
+    //         </div>
+    //     `
+    //     console.log(data[i].id);
+    //     placeholder.innerHTML += markup;
+    //
+    //   })})
+    };
